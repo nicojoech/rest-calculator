fe75ba8d239163f0703a15f36c4c32de6ee4e92a
Nico Jöch <wi21b032@technikum-wien.at>
2022-06-02 12:51:44 +0200

    feature/abs

--- src/main/java/com/example/restcalculator/CalculatorController.java
+++ src/main/java/com/example/restcalculator/CalculatorController.java
@@ -16,7 +16,46 @@ public class CalculatorController {
     }
 
     @RequestMapping("/mul")
-    int multiply(@RequestParam int a, @RequestParam int b){
+    int multiply(@RequestParam int a, @RequestParam int b) {
         return a * b;
+    }
+
+    @RequestMapping("/quad")
+    public int quad(
+            @RequestParam int a
+    ) {
+        return a * a;
+    }
+
+    @RequestMapping("/abs")
+    public int abs(
+            @RequestParam int a
+    ) {
+        return Math.abs(a);
+    }
+
+    @RequestMapping("/exp")
+    public double exp(
+            @RequestParam double a,
+            @RequestParam double b
+    ) {
+        if (b == 0) {
+            return 1;
+        } else if (b < 0) {
+            return 1 / exp2(a, b);
+        } else {
+            return exp2(a, b);
+        }
+    }
+
+    public double exp2(double a, double b) {
+        int val = 1;
+        if (b < 0) {
+            b *= -1;
+        }
+        for (int i = 0; i < b; i++) {
+            val *= a;
+        }
+        return val;
     }
 }
